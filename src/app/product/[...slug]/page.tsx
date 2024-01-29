bdd28be04de07baff5a770944310d1a411c3ff1b
“FirmanFrezyPradana” <dana.firman16@gmail.com>
2024-01-29 15:32:12 +0700

    6. data fetching

--- src/app/product/[...slug]/page.tsx
+++ src/app/product/[...slug]/page.tsx
@@ -1,17 +1,40 @@
-//untuk mengangkap 1 props saja
-type DetailProductPageProps = {params : {slug : string}}
-// untuk menangkap banyak props pada folder [slug] ubah menjadi [...slug] maka seluruh parameter akan mudak ditangkap
-export default function DetailProduk(props:DetailProductPageProps){
-    const {params} = props;
-    return(
-        <div className="">
+// //untuk mengangkap 1 props saja
+// type DetailProductPageProps = {params : {slug : string}}
+// // untuk menangkap banyak props pada folder [slug] ubah menjadi [...slug] maka seluruh parameter akan mudak ditangkap
+// export default function DetailProduk(props:DetailProductPageProps){
+//     const {params} = props;
+//     return(
+//         <div className="">
 
-            <h1>
-            detail produk page
-            </h1>
-            <p>{params.slug[0]}</p>
-            <p>{params.slug[1]}</p>
-            <p>{params.slug[2]}</p>
-            </div>
-    )
+//             <h1>
+//             detail produk page
+//             </h1>
+//             <p>{params.slug[0]}</p>
+//             <p>{params.slug[1]}</p>
+//             <p>{params.slug[2]}</p>
+//             </div>
+//     )
+// }
+type ProductPageProps = {params : {slug : string[]}}
+async function getData() {
+    const res = await fetch("https://fakestoreapi.com/products");
+    if(!res.ok){
+        throw new Error("Filed to fetch data")
+    }
+    return res.json();
 }
+export default async function ProductPage(props : ProductPageProps) {
+    const {params} = props;
+    const products = await getData();
+    // console.log(products);
+    return(
+        <div>
+            {/* <h1>{params.slug ? "Detail Product Page" : "Product page"}</h1> */}
+            {products.length > 0 && products.map((product:any)=>{
+                <div key={product.id}>
+                    <h4>(product.title)</h4>
+                </div>
+            })}
+        </div>
+    );
+};
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,5 +1,59 @@
-export default function Produk(){
-    return(
-        <div className=""><h1>Produk page</h1></div>
-    )
+"use client";
+type ProductPageProps = { params: { slug: string[] } };
+async function getData() {
+  //const res = await fetch("https://fakestoreapi.com/products");// fetch data dari luar
+  const res = await fetch("http://localhost:3000/api/product",{
+    cache:"no-store",
+  });// fetch data dari localhost
+  if (!res.ok) {
+    throw new Error("Filed to fetch data");
+  }
+  return res.json();
+}
+export default async function ProductPage(props: ProductPageProps) {
+  const { params } = props;
+  const products = await getData();
+  // console.log(products);
+  return (
+    <div className="grid grid-cols-4 mt-5 mx-4">
+    {/* ambl data dari local */}
+      {products.data.length > 0 &&
+        products.data.map((product: any) => (
+
+        //ambil data dari internet
+    //   {products.length > 0 &&
+    //     products.map((product: any) => (
+
+            <div key={product.id} className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 my-3">
+              <a href="#">
+                <img
+                  className="p-8 rounded-t-lg object-cover h-96 w-full"
+                  src={product.image}
+                  alt="product image"
+                />
+              </a>
+              <div className="px-5 pb-5">
+                <a href="#">
+                  <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                    {product.title}
+                  </h5>
+                </a>
+
+                <div className="flex items-center justify-between">
+                  <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                    ${product.price}
+                  </span>
+                  <a
+                    href="#"
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Add to cart
+                  </a>
+                </div>
+              </div>
+            </div>
+
+        ))}
+    </div>
+  );
 }
