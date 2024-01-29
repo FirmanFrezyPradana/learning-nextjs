e2e1b1692b0dde43dfdea3050c95ad340020a036
“FirmanFrezyPradana” <dana.firman16@gmail.com>
2024-01-29 23:59:19 +0700

    9. middleware

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const isLogin = false;
+    if(!isLogin){
+        return NextResponse.redirect(new URL("/Login", request.url));
+    }
+
+    //cara satu satu
+//   if (request.nextUrl.pathname.startsWith("/about")) {
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+//   if (request.nextUrl.pathname.startsWith("/product")) {
+//     return NextResponse.redirect(new URL("/Login", request.url));
+//   }
+
+}
+//CARA CEPAT
+//matcher => memfilter url yang ingin ditampilkan
+export const config = {
+    matcher : ["/dashboard/:path*","/about/:path*"],
+}
