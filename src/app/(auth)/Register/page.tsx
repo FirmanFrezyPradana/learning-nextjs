2ce1b72ab2e38e52687357dc7a8abe5dbbf010f6
“FirmanFrezyPradana” <dana.firman16@gmail.com>
2024-02-05 01:09:27 +0700

    14. Auth Register

--- src/app/(auth)/Register/page.tsx
+++ src/app/(auth)/Register/page.tsx
@@ -1,26 +1,52 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
-export default function Register() {
-
-    return (
-      <div className="h-screen w-100 flex justify-center items-center">
+export default function RegisterPage() {
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+  const [loading, setIsLoading] = useState(false);
+  const handleRegister = async (e: any) => {
+    e.preventDefault();
+    setError("");
+    setIsLoading(true);
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        fullname: e.target.fullname.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }),
+    });
+    if (res.status === 200) {
+      e.target.reset();
+      push("/Login");
+      setIsLoading(false);
+    } else {
+      setError("Email already exist");
+      setIsLoading(false);
+    }
+  };
+  return (
+    <div className="h-screen w-100 flex justify-center items-center flex-col">
+      {error && <div className="text-rose-500 font-bold mb-3">{error}</div>}
       <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form className="space-y-6" action="#">
+        <form className="space-y-6" onSubmit={(e) => handleRegister(e)}>
           <h3 className="text-xl font-medium text-gray-900 dark:text-white">
             Register in to our platform
           </h3>
           <div>
             <label
-              htmlFor="email"
+              htmlFor="fullname"
               className="text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300"
             >
               Yout FullName
             </label>
             <input
-              type="email"
-              name="email"
-              id="email"
+              type="text"
+              name="fullname"
+              id="fullname"
               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               placeholder="your name"
               required
@@ -58,33 +84,6 @@ export default function Register() {
               required
             />
           </div>
-          <div className="flex items-start">
-            <div className="flex items-start">
-              <div className="flex items-center h-5">
-                <input
-                  id="remember"
-                  aria-describedby="remember"
-                  type="checkbox"
-                  className="bg-gray-50 border border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                  required
-                />
-              </div>
-              <div className="text-sm ml-3">
-                <label
-                  htmlFor="remember"
-                  className="font-medium text-gray-900 dark:text-gray-300"
-                >
-                  Remember me
-                </label>
-              </div>
-            </div>
-            <a
-              href="#"
-              className="text-sm text-blue-700 hover:underline ml-auto dark:text-blue-500"
-            >
-              Lost Password?
-            </a>
-          </div>
           <button
             type="submit"
             className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -103,5 +102,5 @@ export default function Register() {
         </form>
       </div>
     </div>
-    );
-  }
+  );
+}
--- src/lib/firebase/sevice.ts
+++ src/lib/firebase/sevice.ts
@@ -1,5 +1,16 @@
-import { collection, doc, getDoc, getDocs, getFirestore } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
 import app from "./init";
+import bcrypt from "bcrypt";
+import { error } from "console";
 
 const firestore = getFirestore(app);
 
@@ -16,4 +27,34 @@ export async function retrieveDataById(collectionName: string, id: string) {
 
   const data = snapshot.data();
   return data;
+}
+
+export async function register(data: {
+  fullname: string;
+  email: string;
+  password: string;
+  role?: string;
+}) {
+  const q = query(
+    collection(firestore, "user"),
+    where("email", "==", data.email)
+  );
+  const snapshot = await getDocs(q);
+  const users = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+
+  if (users.length > 0) {
+    return { status: false, message: "Email already exist", statusCode: 400 };
+  } else {
+    data.role = "admin";
+    data.password = await bcrypt.hash(data.password, 10);
+    try {
+      await addDoc(collection(firestore, "user"), data);
+      return { status: true, message: "Register succes", statusCode: 200 };
+    } catch (error) {
+      return { status: false, message: "Register failed", statusCode: 400 };
+    }
+  }
 }
